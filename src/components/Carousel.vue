361d230b2c4dd761ed279101bfd28922d766d17d
sk0v0roda <67000954+sk0v0roda@users.noreply.github.com>
2020-12-02 20:21:03 +0300

    initial commit

---
+++ src/components/Carousel.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div class="top-string">
+      <h1>
+        Наши товары
+      </h1>
+      <h2>
+        Купите...
+      </h2>
+    </div>
+    <div class="container">
+      <div class="row-12">
+        <carousel :center="true" :loop="true">
+
+          <img src="../assets/carousel_pics/tort1.jpg" width="300" height="300">
+          <img src="../assets/carousel_pics/tort2.jpg" width="300" height="300">
+          <img src="../assets/carousel_pics/tort3.jpg" width="300" height="300">
+          <img src="../assets/carousel_pics/tort4.jpg" width="300" height="300">
+          <img src="../assets/carousel_pics/tort5.jpg" width="300" height="300">
+
+        </carousel>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+
+import carousel from 'vue-owl-carousel'
+
+export default {
+  components: {carousel},
+}
+
+</script>
+<style scoped>
+img {
+  padding: 30px;
+}
+
+h1, h2 {
+  text-align: center;
+}
+
+h2 {
+  color: grey;
+}
+.top-string {
+  padding-top: 50px;
+}
+
+.container {
+  border: 2px solid black;
+  border-radius: 10px;
+}
+</style>
---
+++ src/components/Promo.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="promo-main">
+    <div class="container">
+      <div class="row">
+        <div class="col-4" v-for="card in cards" :key="card.index">
+          <div class="card" style="width: 18rem;">
+            <img src="..\assets\img-template.svg" class="card-img-top" alt="img">
+            <div class="card-body">
+              <h5 class="card-title">{{ card.title }}</h5>
+              <span class="badge badge-dark">Тортик</span>
+              <span class="badge badge-dark">Авторучка</span>
+              <span class="badge badge-dark">Канделябр</span>
+              <p class="card-text">{{ card.description }}</p>
+              <a href="#" class="btn btn-primary buy"><span class="button-text">Купить</span></a>
+            </div>
+          </div>
+        </div>
+        <div class="col-4"></div>
+        <div class="btn btn-primary catalogue col-4">В каталог</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cards: [{
+        title: 'Торты за 50%, вы не ослышались',
+        description: `Психоанализ порождает гендер. Скиннер выдвинул концепцию "оперантного",
+              подкрепляемого
+              научения, в которой предсознательное отталкивает потребительский системный анализ.`,
+        src: '..\\assets\\png-transparent-cupcake-computer-icons-cake-pop-cupcake-icon-food-cake-artwork.png'
+      }, {
+        title: 'У нас нет сахара вообще нет не купили забыли',
+        description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur deleniti fugit
+        incidunt ipsam laudantium nemo numquam, officia omnis optio quisquam ratione tempora. At consequatur
+        doloremque ducimus quis sint tenetur, vel!`,
+        src: '..\\assets\\png-transparent-cupcake-computer-icons-cake-pop-cupcake-icon-food-cake-artwork.png'
+      }, {
+        title: 'У нас нет сахара вообще нет не купили забыли',
+        description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur deleniti fugit
+        incidunt ipsam laudantium nemo numquam, officia omnis optio quisquam ratione tempora. At consequatur
+        doloremque ducimus quis sint tenetur, vel!`,
+        src: '..\\assets\\png-transparent-cupcake-computer-icons-cake-pop-cupcake-icon-food-cake-artwork.png'
+      },],
+    }
+  }
+}
+</script>
+
+<style scoped>
+.buy, .catalogue {
+  background: #555BEC;
+  width: 100%;
+  height: 64px;
+  margin-left: auto;
+  margin-right: auto;
+  align-items: center;
+  text-align: center;
+}
+
+.catalogue {
+  width: 300px;
+  align-self: center;
+  text-transform: uppercase;
+  margin: 30px;
+}
+
+.card {
+  margin-top: 20px;
+}
+
+.button-text {
+  font-family: 'Open-Sans';
+  font-size: 20px;
+  text-transform: uppercase;
+}
+
+span.badge {
+  padding: 7px;
+  text-transform: uppercase;
+  font-size: 18px;
+  margin: 4px;
+  color: #333333;
+  background: #C0C0C0;
+}
+
+.promo-main {
+  margin: 5px;
+  padding: 30px;
+}
+</style>
