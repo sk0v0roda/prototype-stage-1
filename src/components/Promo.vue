8ed1e26bb528b94f1abf7b55374416d2e66a4738
sk0v0roda <steponist@mail.ru>
2020-12-06 19:41:28 +0300

    Подключены импортированные изображения в блоке акций

--- src/components/Promo.vue
+++ src/components/Promo.vue
@@ -3,23 +3,15 @@
     <div class="container">
       <div class="row">
         <div class="col-4" v-for="card in cards" :key="card.index">
-          <div class="card border-0">
-            <img
-              src="..\assets\img-template.svg"
-              class="card-img-top mb-3"
-              alt="img"
-            />
-            <div class="card-body p-0">
+          <div class="card" style="width: 18rem;">
+            <img :src="card.img" class="card-img-top" alt="img">
+            <div class="card-body">
               <h5 class="card-title">{{ card.title }}</h5>
-              <div>
-                <span class="badge badge-dark">Тортик</span>
-                <span class="badge badge-dark">Авторучка</span>
-                <span class="badge badge-dark">Канделябр</span>
-              </div>
+              <span class="badge badge-dark">Тортик</span>
+              <span class="badge badge-dark">Авторучка</span>
+              <span class="badge badge-dark">Канделябр</span>
               <p class="card-text">{{ card.description }}</p>
-            </div>
-            <div class="card-footer border-0 p-0">
-              <a href="#" class="btn btn-primary"> Купить </a>
+              <a href="#" class="btn btn-primary buy"><span class="button-text">Купить</span></a>
             </div>
           </div>
         </div>
@@ -31,63 +23,43 @@
 </template>
 
 <script>
+import tort from '../assets/png-transparent-cupcake-computer-icons-cake-pop-cupcake-icon-food-cake-artwork.png'
 export default {
   data() {
     return {
-      cards: [
-        {
-          title: 'Торты за 50%, вы не ослышались',
-          description: `Психоанализ порождает гендер. Скиннер выдвинул концепцию "оперантного",
+      cards: [{
+        title: 'Торты за 50%, вы не ослышались',
+        description: `Психоанализ порождает гендер. Скиннер выдвинул концепцию "оперантного",
               подкрепляемого
               научения, в которой предсознательное отталкивает потребительский системный анализ.`,
-          src:
-            '..\\assets\\png-transparent-cupcake-computer-icons-cake-pop-cupcake-icon-food-cake-artwork.png',
-        },
-        {
-          title: 'У нас нет сахара вообще нет не купили забыли',
-          description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur deleniti fugit
+        img: tort
+      }, {
+        title: 'У нас нет сахара вообще нет не купили забыли',
+        description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur deleniti fugit
         incidunt ipsam laudantium nemo numquam, officia omnis optio quisquam ratione tempora. At consequatur
         doloremque ducimus quis sint tenetur, vel!`,
-          src:
-            '..\\assets\\png-transparent-cupcake-computer-icons-cake-pop-cupcake-icon-food-cake-artwork.png',
-        },
-        {
-          title: 'У нас нет сахара вообще нет не купили забыли',
-          description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur deleniti fugit
+        img: tort
+      }, {
+        title: 'У нас нет сахара вообще нет не купили забыли',
+        description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur deleniti fugit
         incidunt ipsam laudantium nemo numquam, officia omnis optio quisquam ratione tempora. At consequatur
         doloremque ducimus quis sint tenetur, vel!`,
-          src:
-            '..\\assets\\png-transparent-cupcake-computer-icons-cake-pop-cupcake-icon-food-cake-artwork.png',
-        },
-      ],
-    };
-  },
-};
+        img: tort
+      },],
+    }
+  }
+}
 </script>
 
 <style scoped>
-@import url('../root.css');
-
-.buy,
-.catalogue {
-  background: #555bec;
+.buy, .catalogue {
+  background: #555BEC;
   width: 100%;
   height: 64px;
   margin-left: auto;
   margin-right: auto;
   align-items: center;
   text-align: center;
-}
-
-.btn-primary {
-  font-family: 'Open Sans', sans-serif;
-  font-size: 20px;
-  text-transform: uppercase;
-  font-weight: 800;
-  font-size: 20px;
-  width: 100%;
-  background: #555bec;
-  padding: 20px 0;
 }
 
 .catalogue {
@@ -101,13 +73,19 @@ export default {
   margin-top: 20px;
 }
 
+.button-text {
+  font-family: 'Open-Sans';
+  font-size: 20px;
+  text-transform: uppercase;
+}
+
 span.badge {
   padding: 7px;
   text-transform: uppercase;
   font-size: 18px;
   margin: 4px;
   color: #333333;
-  background: #c0c0c0;
+  background: #C0C0C0;
 }
 
 .promo-main {
