5cab104930eb622ee64dfe34e336eae49df0e02e
sk0v0roda <steponist@mail.ru>
2020-12-13 17:10:13 +0300

    добавлена рыба с контентом

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,11 @@
 <template>
-  <div>
-    <app-carousel/>
-    <app-promo/>
-    <app-about/>
-    <app-order/>
-  </div>
+
 </template>
 
 <script>
-import About from './components/About.vue'
-import Order from './components/Order.vue'
-import Carousel from './components/Carousel.vue'
-import Promo from './components/Promo.vue'
 
 export default {
-  components: {
-    appAbout: About,
-    appOrder: Order,
-    appCarousel: Carousel,
-    appPromo: Promo
-  }
+
 }
+
 </script>
